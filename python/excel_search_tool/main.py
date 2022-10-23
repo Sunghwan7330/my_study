ff027529b89521c121dc6f622635f4afaab229db
Sunghwan <chak1212@naver.com>
2022-10-23 20:48:41 +0900

    python 엑셀 데이터 검색 툴업로드

---
+++ python/excel_search_tool/main.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import json
+
+CONFIG_FILE = 'config.json'
+KEY_EXCEL_FILE = 'excel_file'
+KEY_SEARCH_FILE = 'search_list_file'
+KEY_SELECT_SHEET = 'select_sheet'
+KEY_COLUMN = 'key_column'
+KEY_VALUE_INDEX_ARR = 'value_index_arr'
+KEY_WRITE_FILE = 'write_file'
+KEY_DEBUG_MESSAGE = 'debug_message'
+
+debug_flag = False;
+
+def readSearchList(filename):
+    res = []
+    f = open(filename, 'r')
+    while True:
+        line = f.readline()
+        if not line: break
+        res.append(line.replace('\n', ''))
+    f.close()
+    return res
+
+def readConfig():
+    with open(CONFIG_FILE) as f:
+        conf = json.load(f)
+
+    return conf
+
+def print_debug(str):
+    if debug_flag:
+        print(str)
+
+def getResultFromExcel(excel_file, sheet_name, key_col, search_list, write_file_name):
+    res = ''
+    print_debug('* Load Excel')
+    df = pd.read_excel(excel_file, sheet_name=[sheet_name])
+    print_debug('  - Load Sueecss')
+    sheet = df['Sheet1']
+
+    sheet[sheet[key_col].isin(search_list)].to_excel(write_file_name)
+
+    return res
+
+def main():
+    conf = readConfig()
+    global debug_flag
+    debug_flag = conf[KEY_DEBUG_MESSAGE]
+    getResultFromExcel(conf[KEY_EXCEL_FILE],
+                       conf[KEY_SELECT_SHEET],
+                       conf[KEY_COLUMN],
+                       readSearchList(conf[KEY_SEARCH_FILE]),
+                       conf[KEY_WRITE_FILE]
+                       )
+    print_debug('* Finish!')
+    return
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
