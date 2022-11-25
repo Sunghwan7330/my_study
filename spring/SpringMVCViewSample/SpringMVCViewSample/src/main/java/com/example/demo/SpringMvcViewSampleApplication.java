e4f4e2f41ac2c6050844f76d100a855771cc6c81
Sunghwan <chak1212@naver.com>
2022-11-25 21:43:35 +0900

    spring MVC View sample 업로드

---
+++ spring/SpringMVCViewSample/SpringMVCViewSample/src/main/java/com/example/demo/SpringMvcViewSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcViewSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcViewSampleApplication.class, args);
+	}
+
+}
