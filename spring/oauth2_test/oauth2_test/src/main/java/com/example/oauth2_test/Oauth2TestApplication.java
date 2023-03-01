e74c6f1df081a26018fffea93b5f35fd96e44016
Sunghwan <chak1212@naver.com>
2023-03-01 21:41:35 +0900

    spring oauth2 샘플프로젝트 초안 업로드

---
+++ spring/oauth2_test/oauth2_test/src/main/java/com/example/oauth2_test/Oauth2TestApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2TestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2TestApplication.class, args);
+	}
+
+}
---
+++ spring/oauth2_test/oauth2_test/src/test/java/com/example/oauth2_test/Oauth2TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2TestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
