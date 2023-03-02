47216da3b8a907bfd86e58a747d5a7e88d84192e
Sunghwan <chak1212@naver.com>
2023-03-02 21:38:38 +0900

    Sprint Oauth2 로그인 추가

--- spring/oauth2_test/oauth2_test/src/main/java/com/example/oauth2_test/controller/MainController.java
+++ spring/oauth2_test/oauth2_test/src/main/java/com/example/oauth2_test/controller/MainController.java
@@ -1,13 +1,18 @@
 package com.example.oauth2_test.controller;
 
-
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 @Controller
+@Log4j2
 public class MainController {
     @GetMapping({"/", "/index"})
-    public String getMain() {
+    public String getMain(@AuthenticationPrincipal OAuth2User oAuth2User) {
+        log.info("로그인 된 유저:  " + oAuth2User);
+
         return "main";
     }
 }
