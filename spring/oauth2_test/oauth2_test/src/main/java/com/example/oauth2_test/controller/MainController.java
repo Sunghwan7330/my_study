675e84aafce4de25393ef39d67bef1b9810803a0
Sunghwan <chak1212@naver.com>
2023-03-05 14:36:21 +0900

    spring oauth2 로그인 시 정보 출력 추가

--- spring/oauth2_test/oauth2_test/src/main/java/com/example/oauth2_test/controller/MainController.java
+++ spring/oauth2_test/oauth2_test/src/main/java/com/example/oauth2_test/controller/MainController.java
@@ -1,19 +1,30 @@
 package com.example.oauth2_test.controller;
 
+import com.example.oauth2_test.dto.OAuthAttributes;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 
+import java.util.Iterator;
+import java.util.Map;
+
 @Controller
 @Log4j2
 public class MainController {
-    @GetMapping({"/", "/index"})
-    public String getMain(@AuthenticationPrincipal OAuth2User oAuth2User) {
+    @GetMapping({"/", "/index", "info"})
+    public String getMain(@AuthenticationPrincipal OAuth2User oAuth2User, Model model) {
         log.info("로그인 된 유저:  " + oAuth2User);
 
-        return "index";
+        if (oAuth2User == null)
+            return "index";
+
+        OAuthAttributes attributes = OAuthAttributes.of("twitch", "", oAuth2User.getAttributes());
+        model.addAttribute("user_info", attributes);
+
+        return "info";
     }
 
     // 제대로 로그인이 되었는지 확인하는 요청
--- spring/oauth2_test/oauth2_test/src/main/java/com/example/oauth2_test/dto/OAuthAttributes.java
+++ spring/oauth2_test/oauth2_test/src/main/java/com/example/oauth2_test/dto/OAuthAttributes.java
@@ -3,19 +3,23 @@ package com.example.oauth2_test.dto;
 import com.example.oauth2_test.user.User;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 import java.util.Map;
 
 @Data
+@Log4j2
 @NoArgsConstructor
 public class OAuthAttributes {
+    private String connectPlatform;
     private Map<String, Object> attributes;
     private String nameAttributeKey;
     private String name;
     private String email;
     private String picture;
 
-    public OAuthAttributes(Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
+    public OAuthAttributes(String connectPlatform, Map<String, Object> attributes, String nameAttributeKey, String name, String email, String picture) {
+        this.connectPlatform = connectPlatform;
         this.attributes = attributes;
         this.nameAttributeKey = nameAttributeKey;
         this.name = name;
@@ -27,23 +31,24 @@ public class OAuthAttributes {
     // 여기서 registrationId는 OAuth2 로그인을 처리한 서비스 명("google","kakao","naver"..)이 되고,
     // userNameAttributeName은 해당 서비스의 map의 키값이 되는 값이됩니다. {google="sub", kakao="id", naver="response"}
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes) {
-        //TODO 추후 확장
-        /*
-        if (registrationId.equals("kakao")) {
+        log.info("[OAuthAttributes] registrationId : " + registrationId);
+
+        if (registrationId.equals("twitch")) {
+            return ofTwitch(userNameAttributeName, attributes);
+        } else if (registrationId.equals("kakao")) {
             return ofKakao(userNameAttributeName, attributes);
         } else if (registrationId.equals("naver")) {
             return ofNaver(userNameAttributeName,attributes);
         }
         return ofGoogle(userNameAttributeName, attributes);
-        */
-        return ofTwitch(userNameAttributeName, attributes);
     }
 
     private static OAuthAttributes ofKakao(String userNameAttributeName, Map<String, Object> attributes) {
         Map<String, Object> kakao_account = (Map<String, Object>) attributes.get("kakao_account");  // 카카오로 받은 데이터에서 계정 정보가 담긴 kakao_account 값을 꺼낸다.
         Map<String, Object> profile = (Map<String, Object>) kakao_account.get("profile");   // 마찬가지로 profile(nickname, image_url.. 등) 정보가 담긴 값을 꺼낸다.
 
-        return new OAuthAttributes(attributes,
+        return new OAuthAttributes("Kakao",
+                attributes,
                 userNameAttributeName,
                 (String) profile.get("nickname"),
                 (String) kakao_account.get("email"),
@@ -53,7 +58,8 @@ public class OAuthAttributes {
     private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
         Map<String, Object> response = (Map<String, Object>) attributes.get("response");    // 네이버에서 받은 데이터에서 프로필 정보다 담긴 response 값을 꺼낸다.
 
-        return new OAuthAttributes(attributes,
+        return new OAuthAttributes("Naver",
+                attributes,
                 userNameAttributeName,
                 (String) response.get("name"),
                 (String) response.get("email"),
@@ -62,7 +68,8 @@ public class OAuthAttributes {
 
     private static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes) {
 
-        return new OAuthAttributes(attributes,
+        return new OAuthAttributes("Google",
+                attributes,
                 userNameAttributeName,
                 (String) attributes.get("name"),
                 (String) attributes.get("email"),
@@ -70,7 +77,8 @@ public class OAuthAttributes {
     }
 
     private static OAuthAttributes ofTwitch(String userNameAttributeName, Map<String, Object> attributes) {
-        return new OAuthAttributes(attributes,
+        return new OAuthAttributes("Twitch",
+                attributes,
                 userNameAttributeName,
                 (String) attributes.get("preferred_username"),
                 (String) attributes.get("sub"),
