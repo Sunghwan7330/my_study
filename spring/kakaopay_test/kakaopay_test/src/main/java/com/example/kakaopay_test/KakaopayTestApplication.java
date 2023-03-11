49d79c1dcc885bf7b3b270e9860011d2db0a8eec
Sunghwan <chak1212@naver.com>
2023-03-11 20:09:01 +0900

    Spring kakaopay 연동 테스트 프로젝트 추가

---
+++ spring/kakaopay_test/kakaopay_test/src/main/java/com/example/kakaopay_test/KakaopayTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.kakaopay_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaopayTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaopayTestApplication.class, args);
+	}
+
+}
---
+++ spring/kakaopay_test/kakaopay_test/src/test/java/com/example/kakaopay_test/KakaopayTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kakaopay_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaopayTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
