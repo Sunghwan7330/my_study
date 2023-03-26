b87b2fd2cb5abd9b59404adae9eba56d9949877c
Sunghwan <chak1212@naver.com>
2023-03-26 18:57:52 +0900

    spring websocket 테스트 추가

---
+++ spring/websockettest/websockettest/src/main/java/com/example/websockettest/WebsockettestApplication.java
@@ -0,0 +1,13 @@
+package com.example.websockettest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsockettestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsockettestApplication.class, args);
+	}
+
+}
---
+++ spring/websockettest/websockettest/src/test/java/com/example/websockettest/WebsockettestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.websockettest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsockettestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
