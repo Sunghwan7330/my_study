2f90a6afc41e63efe8a94d308785871dd412ce5a
Sunghwan <chak1212@naver.com>
2023-03-30 22:22:01 +0900

    sprint alertbox_test 프로젝트추가

---
+++ spring/obs_alertbox_test/obs_alertbox_test/src/main/java/com/example/obs_alertbox_test/ObsAlertboxTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.obs_alertbox_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObsAlertboxTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObsAlertboxTestApplication.class, args);
+	}
+
+}
---
+++ spring/obs_alertbox_test/obs_alertbox_test/src/test/java/com/example/obs_alertbox_test/ObsAlertboxTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.obs_alertbox_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObsAlertboxTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
