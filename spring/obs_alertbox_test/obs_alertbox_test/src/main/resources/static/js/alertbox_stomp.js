a7ac09904f30b6cc11886547f0843782cb5439b1
Sunghwan <chak1212@naver.com>
2023-04-02 20:54:34 +0900

    sprint alertbox fade in/out 효과 추가

--- spring/obs_alertbox_test/obs_alertbox_test/src/main/resources/static/js/alertbox_stomp.js
+++ spring/obs_alertbox_test/obs_alertbox_test/src/main/resources/static/js/alertbox_stomp.js
@@ -1,9 +1,11 @@
 var stompClient;
 var token = getTokenFromPathname()
 
+var fadeIntervalID = 0;
+
 window.onload = function(){
-        openSocket()
-    }
+    openSocket()
+}
 
 function getTokenFromPathname() {
     split_path = window.location.pathname.split("/") // value : /alertbox/{token}
@@ -22,9 +24,14 @@ function openSocket(){
     stompClient.connect({}, function(frame) {
         console.log('Connected: ' + frame);
         stompClient.subscribe('/donationinfo/' + token, function(response) {
+            var div = document.getElementById("main_frame");
+            div.style.opacity=0;
+
             donationInfo = JSON.parse(response.body)
             setImage(donationInfo.image_path)
             setMessage(donationInfo.donation_message)
+            fadeIntervalID = setInterval(show,100);
+            setTimeout(end,5000);
         });
     })
 }
@@ -42,3 +49,32 @@ function setMessage(msg) {
     document.getElementById("donationMessage").innerText = msg;
 }
 
+function show(){
+    var div = document.getElementById("main_frame");
+    opacity = Number(window.getComputedStyle(div).getPropertyValue("opacity"));
+    if(opacity<1){
+        opacity = opacity+0.1;
+        div.style.opacity=opacity;
+    }
+    else{
+        clearInterval(fadeIntervalID);
+
+    }
+}
+
+function hide(){
+    var div = document.getElementById("main_frame");
+    opacity = Number(window.getComputedStyle(div).getPropertyValue("opacity"));
+
+    if(opacity>0){
+        //Fade out 핵심 부분
+        opacity = opacity-0.1;
+        div.style.opacity=opacity;
+    }
+    else{
+        clearInterval(fadeIntervalID);
+    }
+}
+function end() {
+    fadeIntervalID = setInterval(hide,100);
+}
