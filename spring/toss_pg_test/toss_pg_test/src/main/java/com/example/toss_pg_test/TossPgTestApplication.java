82cdaf13073b80328673e308c4ac4edd0f1a4a5f
Sunghwan <chak1212@naver.com>
2023-04-08 16:59:07 +0900

    spring toss pt 테스트  프로젝트추가

---
+++ spring/toss_pg_test/toss_pg_test/src/main/java/com/example/toss_pg_test/TossPgTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.toss_pg_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TossPgTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TossPgTestApplication.class, args);
+	}
+
+}
---
+++ spring/toss_pg_test/toss_pg_test/src/test/java/com/example/toss_pg_test/TossPgTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.toss_pg_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TossPgTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
