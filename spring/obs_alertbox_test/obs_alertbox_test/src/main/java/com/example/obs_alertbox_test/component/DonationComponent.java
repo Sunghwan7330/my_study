92f857e66783a9d31a05f45c85ae1a0b81ba3ab0
Sunghwan <chak1212@naver.com>
2023-05-01 21:59:13 +0900

    feat: obs alertbox 큐 입력 후 시간에 맞게 출력

--- spring/obs_alertbox_test/obs_alertbox_test/src/main/java/com/example/obs_alertbox_test/component/DonationComponent.java
+++ spring/obs_alertbox_test/obs_alertbox_test/src/main/java/com/example/obs_alertbox_test/component/DonationComponent.java
@@ -1,6 +1,7 @@
 package com.example.obs_alertbox_test.component;
 
 import com.example.obs_alertbox_test.TextToSpeech;
+import com.example.obs_alertbox_test.data_structure.DonationInfoQueue;
 import com.example.obs_alertbox_test.dto.DonationInfoDto;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,10 +11,13 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletContext;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
 
 @Log4j2
 @Component
@@ -24,36 +28,42 @@ public class DonationComponent {
     @Autowired
     private ServletContext servletContext;
 
-    private HashMap<String, Queue<DonationInfoDto>> donationListHashmap;
+    private HashMap<String, DonationInfoQueue> donationListHashmap;
     private boolean sendDonationFlag = false;
+
+    private static final long DEFAULT_DURATION_TIME = 3000;
+    private static final long DURATION_GAP_TIME = 1000;
+    private static final long MAX_DURATION_TIME = 20000;
 
     public DonationComponent() {
         donationListHashmap = new HashMap<>();
     }
 
     public void inputDonationInfo(DonationInfoDto donationInfo) {
-        Queue<DonationInfoDto> donationQueue;
+        DonationInfoQueue donationQueue;
         String token = donationInfo.getToken();
 
         if (donationListHashmap.containsKey(token)) {
             donationQueue = donationListHashmap.get(token);
         } else {
-            donationQueue = new LinkedList<DonationInfoDto>();
+            donationQueue = new DonationInfoQueue();
             donationListHashmap.put(token, donationQueue);
         }
 
-        donationQueue.offer(donationInfo);
+        donationQueue.enqueue(donationInfo);
     }
 
     public synchronized void processSendingDonation() {
         sendDonationFlag = true;
         for (String key : donationListHashmap.keySet()) {
-            log.info("token : " + key);
-            Queue<DonationInfoDto> queue = donationListHashmap.get(key);
+            DonationInfoQueue queue = donationListHashmap.get(key);
             if (queue.isEmpty()) continue;
 
-            log.info(queue.peek());
-            sendDonation(queue.poll());
+            if (queue.getNextSendTime() < System.currentTimeMillis()) {
+                queue.setSendTime(System.currentTimeMillis());
+                queue.setDurationTime(DEFAULT_DURATION_TIME);
+                sendDonation(queue);
+            }
         }
         sendDonationFlag = false;
     }
@@ -62,14 +72,33 @@ public class DonationComponent {
         return sendDonationFlag;
     }
 
-    @Async
-    public void sendDonation(DonationInfoDto donationInfo) {
-        log.info("request : " + servletContext.getRealPath("/"));
-        String filename = System.currentTimeMillis() + ".mp3";
-        TextToSpeech tts = new TextToSpeech();
+    private long getDurationWithWav(String filePath) throws UnsupportedAudioFileException, IOException {
+        File wavFile = new File(filePath);
         try {
-            tts.synthesizeText(donationInfo.getDonationMessage(), servletContext.getRealPath("/") + filename);
-        } catch (IOException e) {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(wavFile);
+            AudioFormat format = audioInputStream.getFormat();
+            long frames = audioInputStream.getFrameLength();
+            double durationInSeconds = (frames + 0.0) / format.getFrameRate();
+            return (long) (durationInSeconds * 1000);
+        } catch (UnsupportedAudioFileException | IOException e) {
+            e.printStackTrace();
+        }
+        return 0;
+    }
+
+    @Async
+    public void sendDonation(DonationInfoQueue donationInfoQueue) {
+        DonationInfoDto donationInfo = donationInfoQueue.dequeue();
+        log.info("request : " + servletContext.getRealPath("/"));
+        String filename = System.currentTimeMillis() + ".wav";
+        String fullFilePath = servletContext.getRealPath("/") + filename;
+        TextToSpeech tts = new TextToSpeech();
+        long durationTime = 0;
+
+        try {
+            tts.synthesizeText(donationInfo.getDonationMessage(), fullFilePath);
+            durationTime = Math.min(getDurationWithWav(fullFilePath), MAX_DURATION_TIME);
+        } catch (IOException | UnsupportedAudioFileException e) {
             throw new RuntimeException(e);
         }
 
@@ -79,9 +108,10 @@ public class DonationComponent {
         payload.put("donation_sound", "https://assets.mytwip.net/sounds/Coins.mp3");  //TODO 임시로 twip 소리 사용
         payload.put("tts_sound", "http://localhost:8080/audio/" + filename);
         simpMessagingTemplate.convertAndSend("/donationinfo/" + donationInfo.getToken(), payload);
+        donationInfoQueue.addDurationTime(durationTime);
     }
 
-    @Scheduled(fixedDelay = 1000)
+    @Scheduled(fixedDelay = 100)
     public void sendDonationSchedule() {
         if (isSendingDonation()) return;
         processSendingDonation();
---
+++ spring/obs_alertbox_test/obs_alertbox_test/src/main/java/com/example/obs_alertbox_test/data_structure/DonationInfoQueue.java
@@ -0,0 +1,50 @@
+package com.example.obs_alertbox_test.data_structure;
+
+import com.example.obs_alertbox_test.dto.DonationInfoDto;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class DonationInfoQueue {
+    //TODO lock 추가 검토
+    private long sendTime = 0;
+    private long durationTime = 0;
+    private Queue<DonationInfoDto> queue;
+
+
+    public DonationInfoQueue() {
+        queue = new LinkedList<>();
+    }
+
+    public void enqueue(DonationInfoDto donationInfo) {
+        queue.offer(donationInfo);
+    }
+
+    public DonationInfoDto peek() {
+        return queue.peek();
+    }
+
+    public DonationInfoDto dequeue() {
+        return queue.poll();
+    }
+
+    public boolean isEmpty() {
+        return queue.isEmpty();
+    }
+
+    public long getNextSendTime() {
+        return sendTime + durationTime;
+    }
+
+    public void setSendTime(long sendTime) {
+        this.sendTime = sendTime;
+    }
+
+    public void setDurationTime(long durationTime) {
+        this.durationTime = durationTime;
+    }
+
+    public void addDurationTime(long durationTime) {
+        this.durationTime += durationTime;
+    }
+}
